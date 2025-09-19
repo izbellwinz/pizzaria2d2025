ee88c611c3964efe41f2ecfa25b5efbd991e2889
izbellwinz <rm95085@estudante.fieb.edu.br>
2025-09-19 09:42:52 -0300

    pizza

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,12 +1,17 @@
 const Produtoss = () => {
- 
- 
+ // Obejto com a lista de pizzas
+const pizzas = [
+    'Pizza de Muçarela',
+    'Pizza de Clabresa',
+    'Pizza Baiana',
+    'Pizza Portuguesa'
+]
+// Iteração da lista de pizzas
+const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
     return (
         <div>
 <ul>
-    <li>Pizza de Muçarela</li>
-    <li>Pizza de Calabresa</li>
-    <li>Pizza Baiana</li>
+    {listaPizzas}
 </ul>
  
         </div>
